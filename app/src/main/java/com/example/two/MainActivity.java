07f2e04ab6603e8df5490093fdfe0b26e7351683
He Na <1907705205@qq.com>
2020-03-20 21:28:32 +0800

    温度转换程序

--- app/src/main/java/com/example/two/MainActivity.java
+++ app/src/main/java/com/example/two/MainActivity.java
@@ -2,22 +2,38 @@ package com.example.two;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText num;
+    TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num = (EditText) findViewById(R.id.input);
+        res = (TextView) findViewById(R.id.result);
+
+        Button btn = (Button)findViewById(R.id.butt);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        Log.i("main","onClick msg....");
+        String str = num.getText().toString();
+        double  temper = Double.parseDouble(str);
+        double tem = temper * 9 / 5 + 32;
+        res.setText(temper+"℃对应的华氏温度为"+tem+"℉");
 
     }
 
